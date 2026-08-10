bae827f649f852376e63a035127ea9ec9a7c683a
Erce BERBER <119451936+erceberber@users.noreply.github.com>
2026-08-10 17:23:25 +0300

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,7 +5,8 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    ans = np.array(x)
-    return np.where(ans < 0, ans * alpha, ans)
+
+    arr = np.array(x)
+    return np.where(arr < 0, arr * alpha, arr)
     
     pass
