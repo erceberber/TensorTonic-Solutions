bf122f50bf6e557a3d2c0d716df816477ecfe8e0
Erce BERBER <119451936+erceberber@users.noreply.github.com>
2026-08-13 09:05:09 +0300

    Solve binomial-pmf-cdf

---
+++ binomial-pmf-cdf/binomial-pmf-cdf.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.special import comb
+
+def binomial_pmf_cdf(n, p, k):
+    """
+    Compute Binomial PMF and CDF.
+    """
+    # Write code here
+    pmf = comb(n, k) * np.power(p, k) * np.power(1-p, n-k)
+
+    cdf = 0
+
+    for i in range(k+1):
+        cdf += comb(n, i) * np.power(p, i) * np.power(1-p, n-i)
+    
+    return (float(pmf), float(cdf))
+    
+    pass
