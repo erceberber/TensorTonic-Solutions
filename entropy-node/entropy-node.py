4987deba1fd9d34c0897790b4138f7295d915e0d
Erce BERBER <119451936+erceberber@users.noreply.github.com>
2026-09-02 11:54:17 +0300

    Solve entropy-node

--- entropy-node/entropy-node.py
+++ entropy-node/entropy-node.py
@@ -9,19 +9,13 @@ def entropy_node(y: list[int]) -> float:
     uniq = np.unique(y, return_counts=True)
 
     unique_class_count = len(uniq[0])
-    if unique_class_count == 1:
+    if unique_class_count  <= 1:
         return 0.0
         
     n = len(y)
-    
     counts = uniq[1]
-    
-    p = []
-    for i in range(unique_class_count):
-        p.append(counts[i]/n)
+    p = counts / n
 
     return -np.sum(p * np.log2(p))
     
-
     pass
-
